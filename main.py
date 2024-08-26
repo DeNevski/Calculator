b2c2cfdcd8f06eb1f4bb8b251fb880234b852c3b
DeNevski <denerlira@hotmail.com>
2024-08-26 16:48:45 -0300

    main

---
+++ main.py
@@ -0,0 +1,10 @@
+from interface.calculator import Calculator
+
+def main():
+    calculator = Calculator()
+    root = calculator.root
+
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
